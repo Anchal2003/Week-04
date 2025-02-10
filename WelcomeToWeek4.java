a76cd5b366d4536f2f9aa0b612be25ab385552f7
Anchal2003 <sahuanchal22@gmail.com>
2025-02-10 16:31:50 +0530

    [Anchal Sahu] Add Welcome File.

---
+++ WelcomeToWeek4.java
@@ -0,0 +1,7 @@
+//Create a WelcomeToWeek4 to print the welcome statement
+public class WelcomeToWeek4 {
+    public static void main(String[] args) {
+        System.out.println("\"Welcome to the Week-04!\"");
+    }
+
+}
