41f4464697560c9d8fa47fa438f5fbe4b9339a48
Anchal2003 <sahuanchal22@gmail.com>
2025-02-13 12:22:03 +0530

    [Anchal Sahu] Add file to perform junit testing on list interface practice problems.

---
+++ Day02/JavaCollections/src/test/java/com/javacollectiontesting/ListInterfaceTesting.java
@@ -0,0 +1,54 @@
+package com.javacollectiontesting;
+import com.javacollections.day02.listInterface.*;
+import org.junit.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ListInterfaceTesting {
+
+    @Test
+    public void removeDuplicateElemetnTest(){
+
+        //Create a list and set to store the actual and expected result
+        List<Integer> list = new ArrayList<>(Arrays.asList(3, 1, 2, 2, 4));
+        Set<Integer> expected = new LinkedHashSet<>(Arrays.asList(3, 1, 2, 4));
+
+        // Call the method to remove duplicates
+        Set<Integer> result = RemoveDuplicatesWhilePreservingOrder.removeDuplicateElement(list);
+
+        //call the assertEquals
+        assertEquals(expected, result);
+
+    }
+
+
+
+    @Test
+    //Create a Test method to check if the list is reversed correctly for LinkedList
+    public void reverseListTest() {
+        //Create actualList
+        List<Integer> actualList = new LinkedList<>();
+        actualList.add(1);
+        actualList.add(2);
+        actualList.add(3);
+        actualList.add(4);
+        actualList.add(5);
+
+        // Reverse the list
+        ReverseArrayAndLinkedList.reversList(actualList);
+
+        //Create expectedList
+        List<Integer> expectedList = new LinkedList<>();
+        expectedList.add(5);
+        expectedList.add(4);
+        expectedList.add(3);
+        expectedList.add(2);
+        expectedList.add(1);
+
+        // Verify the reversed list
+        assertEquals(expectedList,actualList );
+    }
+}
