45392920679e3c89b8dc804e4bc69f8e0d544119
Anchal2003 <sahuanchal22@gmail.com>
2025-02-13 21:43:16 +0530

    [Anchal Sahu] Add Day03 Java Stream Practice Solution file.

--- WelcomeToWeek4.java
+++
@@ -1,7 +0,0 @@
-//Create a WelcomeToWeek4 to print the welcome statement
-public class WelcomeToWeek4 {
-    public static void main(String[] args) {
-        System.out.println("\"Welcome to the Week-04!\"");
-    }
-
-}
